3467b34b8dea2f499c2d0c471218db723402a7ca
lanafabi <159067317+lanafabi@users.noreply.github.com>
2026-03-20 11:38:03 +0000

    main

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i = 0; i < botoes.length; i = i + 1){
+    botoes[i].onclick = function(){
+        
+    }
+
+}
+console.log(botoes); 
