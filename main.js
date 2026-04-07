9bcd27466da12b4db3b3993c0cb428e259e986d4
lanafabi <159067317+lanafabi@users.noreply.github.com>
2026-04-07 14:17:06 +0000

    regressiva

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const botoes = document.querySelectorAll(".botao");
 
 for(let i = 0; i < botoes.length; i = i + 1){
     botoes[i].onclick = function(){
-        
+        for(let j = 0; j < botoes.length; j = j + 1){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
     }
 
 }
-console.log(botoes); 
+console.log(botoes);
